cc0a566c436f1a6d187212dc052887bb6561b49c
HwanKR <fabronjeon@naver.com>
2024-11-25 09:54:10 +0900

    비동기 처리 오류 수정

--- components/TakeProfitQuantity.js
+++ components/TakeProfitQuantity.js
@@ -1,6 +1,7 @@
 "use client";
 
-import { useState } from "react";
+import "../styles/TakeProfitQuantity.css";
+import { useState, useEffect } from "react";
 
 export default function TakeProfitQuantity() {
   const [winCount, setWinCount] = useState(0); // 초기값 0
@@ -9,57 +10,74 @@ export default function TakeProfitQuantity() {
   const [rR, setRR] = useState(0); // 초기값 0
   const [minTPQuantity, setMinTPQuantity] = useState(0); // 초기값 0
 
+  useEffect(() => {
+    calcMinTPQuantity();
+  }, [winRate, rR]);
+
   function calcWinRate(win, lose) {
     win = parseInt(win, 10) || 0;
     lose = parseInt(lose, 10) || 0;
 
-    if (win + lose === 0) {
+    if (win === 0) {
       setWinRate(0);
     } else {
-      let rate = (win / (win + lose)) * 100;
-      setWinRate(rate.toFixed(2)); // 소수점 2자리 고정
+      let rate = parseFloat(((win / (win + lose)) * 100).toFixed(2));
+      setWinRate(rate);
     }
+  }
 
-    let minQuantity = (100 - winRate) / (winRate * rR);
-    setMinTPQuantity(minQuantity.toFixed(2)); // 소수점 2자리 고정
-    console.log(minTPQuantity);
+  function calcMinTPQuantity() {
+    let minQuantity = parseFloat(
+      (((100 - winRate) / (winRate * rR)) * 100).toFixed(2)
+    );
+    setMinTPQuantity(minQuantity); // 소수점 2자리 고정
   }
 
   return (
     <>
       <div>
-        <div>
-          <input
-            onChange={(e) => {
-              const value = e.target.value;
-              setWinCount(value);
-              calcWinRate(value, loseCount); // 최신 loseCount 전달
-            }}
-            min={0}
-            type="number"
-          />
-          승
-          <input
-            onChange={(e) => {
-              const value = e.target.value;
-              setLoseCount(value);
-              calcWinRate(winCount, value); // 최신 winCount 전달
-            }}
-            min={0}
-            type="number"
-          />
-          패
-          <input
-            onChange={(e) => {
-              const value = e.target.value;
-              setRR(value);
-            }}
-            type="number"
-            step={0.1}
-          />
-          손익비
+        <div className="inputContainer">
+          <div>
+            <input
+              value={winCount}
+              onChange={(e) => {
+                const value = e.target.value;
+                setWinCount(value);
+                calcWinRate(value, loseCount); // 최신 loseCount 전달
+              }}
+              min={0}
+              type="number"
+            />
+            승
+          </div>
+          <div>
+            <input
+              value={loseCount}
+              onChange={(e) => {
+                const value = e.target.value;
+                setLoseCount(value);
+                calcWinRate(winCount, value); // 최신 winCount 전달
+              }}
+              min={0}
+              type="number"
+            />
+            패
+          </div>
+          <div>
+            <input
+              value={rR}
+              onChange={(e) => {
+                const value = e.target.value;
+                setRR(value);
+              }}
+              type="number"
+              step={0.1}
+            />
+            손익비
+          </div>
         </div>
-        <p>승률 : {winRate}%</p>s<p>최소 익절 수량 : {minTPQuantity}%</p>
+        <p>승률 : {winRate}%</p>
+        <p>최소 익절 수량 : {minTPQuantity}%</p>
       </div>
     </>
   );
